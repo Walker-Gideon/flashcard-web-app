620c48494f58e9eb274d7e1828ad736e758ff57b
Walker-Gideon <gideonawortwe3@gmail.com>
2025-07-16 00:59:19 +0000

    Created the landing page and auth folders and set heir layout files

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
 export default function App() {
-  return <div className="flex text-2xl font-bold">Hello</div>;
+  return (
+    <div className="flex text-2xl">
+      <div>Hello World</div>
+    </div>
+  );
 }
