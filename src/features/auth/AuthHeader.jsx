ff44825543e10a39eacf893e3b881c80378de6ae
Walker-Gideon <gideonawortwe3@gmail.com>
2025-07-16 02:51:15 +0000

    Added the authHeader component in the auth Login section

--- src/features/auth/AuthHeader.jsx
+++ src/features/auth/AuthHeader.jsx
@@ -1,3 +1,13 @@
+import Logo from "../../ui/Logo";
+
 export default function AuthHeader() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <Logo logo={true} />
+
+      <p className="medium:text-base middle:text-lg mt-2 text-sm font-bold">
+        WalkWise
+      </p>
+    </div>
+  );
 }
--- src/features/auth/Login.jsx
+++ src/features/auth/Login.jsx
@@ -1,8 +1,12 @@
+import AuthHeader from "./AuthHeader";
+
 export default function Login() {
   return (
     <div className="relative">
       <div className="flex min-h-screen items-center justify-center">
-        <div className="rounded-2xl border border-stone-300 px-6 py-4 shadow-lg"></div>
+        <div className="rounded-2xl border border-stone-300 px-6 py-4 shadow-lg">
+          <AuthHeader />
+        </div>
       </div>
     </div>
   );
