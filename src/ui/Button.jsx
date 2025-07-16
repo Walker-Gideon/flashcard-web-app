7a7fdc1d561a927269d4725a715540aad679c78c
Walker-Gideon <gideonawortwe3@gmail.com>
2025-07-16 01:59:12 +0000

    Adjustment to the button component

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,13 +1,22 @@
+import { useNavigate } from "react-router-dom";
 import { motion } from "motion/react";
 
-export default function Button({ children, onClick, disabled, btnPaddX }) {
+export default function Button({ children, to, disabled, color, btnPaddX }) {
+  const navigate = useNavigate();
+  const startLoadingAndNavigate = (to) => {
+    // setLoading(true);
+    setTimeout(() => {
+      //   setLoading(false);
+      navigate(to);
+    }, 500);
+  };
+
   return (
     <motion.button
       disabled={disabled}
-      onClick={onClick}
+      onClick={startLoadingAndNavigate(to)}
       whileTap={{ y: 1 }}
-      //   medium:text-[0.74rem] text-sm
-      className={`medium:text-[0.74rem] cursor-pointer rounded-sm border bg-stone-950 py-1.5 text-[0.7rem] font-semibold text-slate-200 transition-colors duration-300 hover:bg-slate-900 hover:text-slate-100 ${btnPaddX ? `${btnPaddX}` : `medium:px-6 px-4`}`}
+      className={`medium:text-[0.74rem] cursor-pointer rounded-sm border text-[0.7rem] font-semibold transition-colors duration-300 ${color ? `bg-stone-950 py-1.5 text-white hover:bg-slate-900` : `border-gray-600 py-[5px] font-bold`} ${btnPaddX ? `${btnPaddX}` : `medium:px-6 px-4`}`}
     >
       {children}
     </motion.button>
