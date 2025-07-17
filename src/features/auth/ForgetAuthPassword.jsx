a92428cdb5393d4ac39a39cd776cdac79b19a549
Walker-Gideon <gideonawortwe3@gmail.com>
2025-07-17 02:53:49 +0000

    Added the reset paragraph to the forget password section

--- src/features/auth/ForgetAuthPassword.jsx
+++ src/features/auth/ForgetAuthPassword.jsx
@@ -6,6 +6,22 @@ export default function ForgetAuthPassword() {
       <div className="h-0.5">
         <Loader />
       </div>
+
+      <div className="flex min-h-[95vh] flex-col items-center justify-center">
+        <div className="medium:max-w-3xl px-14">
+          <div className="medium:text-base mb-6 text-sm">
+            <h1 className="medium:text-2xl middle:text-3xl mt-2 mb-2 text-xl font-bold">
+              Reset your password
+            </h1>
+
+            <p>
+              Enter the email you signed up with. We'll send you a link to log
+              in and reset your password. If you signed up with your parent’s
+              email, we’ll send them a link.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
