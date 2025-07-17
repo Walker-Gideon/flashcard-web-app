b7b714a0ced4b3cf6152b2dd1ae2dc33ca11d993
Walker-Gideon <gideonawortwe3@gmail.com>
2025-07-17 04:32:57 +0000

    Update the loader component by adding the spinner

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -2,12 +2,16 @@ import { useLoader } from "../context/LoaderContext";
 
 //  <div className="absolute inset-0 flex items-center justify-center bg-slate-200/20 backdrop-blur-sm">
 
-export default function Loader() {
+export default function Loader({ spin }) {
   const { loading, spinner } = useLoader();
 
-  if (spinner) return <div className="progress bg-black text-black" />;
-
   return (
-    <div>{loading && <div className="progress bg-black text-black" />}</div>
+    <>
+      {spin
+        ? spinner && (
+            <div className="spinner medium:w-8 medium:p-2 w-7 bg-black p-1.5 text-black" />
+          )
+        : loading && <div className="progress bg-black text-black" />}
+    </>
   );
 }
