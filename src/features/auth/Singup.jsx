1dfbafdf5056339b77a0003805f64c60261676d0
Walker-Gideon <gideonawortwe3@gmail.com>
2025-07-17 02:38:39 +0000

    Added the switch to login page button in the sign up section

--- src/features/auth/Singup.jsx
+++ src/features/auth/Singup.jsx
@@ -1,9 +1,25 @@
+import { useNavigate } from "react-router-dom";
+import { useLoader } from "../../context/LoaderContext";
 import Loader from "../../ui/Loader";
 import AuthClose from "./AuthClose";
 import AuthHeader from "./AuthHeader";
 import SignupForm from "./SignupForm";
+import Button from "../../ui/Button";
 
 export default function Singup() {
+  const { setLoading } = useLoader();
+
+  const navigate = useNavigate();
+  const startLoadingAndNavigate = (to) => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      navigate(to);
+    }, 500);
+  };
+
+  const clascName = "text-[0.8rem] font-semibold";
+
   return (
     <div className="relative">
       <div className="h-1">
@@ -15,6 +31,20 @@ export default function Singup() {
         <div className="rounded-xl border border-stone-300 px-6 py-8 shadow-lg">
           <AuthHeader />
           <SignupForm />
+
+          <div className="mt-2 flex items-center justify-center gap-2">
+            <p className="text-[0.8rem] whitespace-nowrap text-stone-400">
+              Already have an account?
+            </p>
+
+            <Button
+              variant="outline"
+              className={clascName}
+              onClick={() => startLoadingAndNavigate("/accounts/signup")}
+            >
+              Log in
+            </Button>
+          </div>
         </div>
       </div>
     </div>
