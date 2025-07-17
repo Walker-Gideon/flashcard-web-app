d34a3cedbd0b6239da5681f60fe9f17f272147aa
Walker-Gideon <gideonawortwe3@gmail.com>
2025-07-17 03:59:49 +0000

    Added the logic for uploading the user image

--- src/context/LoaderContext.jsx
+++ src/context/LoaderContext.jsx
@@ -4,13 +4,22 @@ const LoaderContext = createContext();
 
 function LoaderProvider({ children }) {
   const [loading, setLoading] = useState(false);
+  const [spinner, setSpinner] = useState(false);
+  const [toast, setToast] = useState(false);
+  const [image, setImage] = useState(null);
 
   const value = useMemo(
     () => ({
       loading,
       setLoading,
+      image,
+      setImage,
+      spinner,
+      setSpinner,
+      toast,
+      setToast,
     }),
-    [loading],
+    [loading, image, spinner, toast],
   );
 
   return (
--- src/features/auth/AuthPicture.jsx
+++ src/features/auth/AuthPicture.jsx
@@ -1,3 +1,103 @@
+import { useEffect, useRef, useState } from "react";
+import { LuUser } from "react-icons/lu";
+import { useNavigate } from "react-router-dom";
+import { useLoader } from "../../context/LoaderContext";
+
 export default function AuthPicture() {
-  return <div className="flex min-h-[95vh] items-center justify-center"></div>;
+  const { setLoading, image, setImage, spinner, setSpinner, toast, setToast } =
+    useLoader();
+  const fileInputRef = useRef();
+  const navigate = useNavigate();
+  const [preview, setPreview] = useState(null);
+
+  useEffect(() => {
+    const handlePopState = (e) => {
+      e.preventDefault();
+
+      //   navigate("/accounts/user", { replace: true });
+    };
+
+    window.addEventListener("popstate", handlePopState);
+
+    return () => {
+      window.removeEventListener("popstate", handlePopState);
+    };
+  }, [navigate]);
+
+  const startLoadingAndNavigate = (to) => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      navigate(to);
+    }, 2000);
+  };
+
+  const handleUpload = async () => {
+    if (!image) return;
+    // Later: upload to Firebase Storage & update user profile
+    console.log("Image to upload:", image);
+
+    // loader
+    setSpinner(true);
+    setTimeout(() => {
+      setSpinner(false);
+
+      // Show toast
+      setToast(true);
+
+      // Clear toast after 3 seconds and redirect
+      setTimeout(() => {
+        setToast(false);
+
+        // redirect
+        navigate("/dashboard");
+      }, 1500);
+    }, 2000);
+  };
+
+  const handleButtonClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+    setImage(file);
+    setPreview(URL.createObjectURL(file));
+  };
+
+  return (
+    <div className="flex min-h-[95vh] items-center justify-center">
+      <div className="rounded-xl border border-stone-300 px-6 py-8 shadow-lg">
+        <div className="medium:w-80 flex w-70 flex-col items-center">
+          <h1 className="medium:text-base middle:text-lg mt-2 mb-10 text-center text-sm font-bold">
+            Set Up Your Profile Photo
+          </h1>
+
+          <label
+            className={`cursor-pointer rounded-full bg-stone-400/30 transition-colors duration-300 ${image ? `p-0` : `p-6`}`}
+          >
+            {preview ? (
+              <img
+                src={preview}
+                alt="preview"
+                className={`rounded-full object-cover ${image ? `h-35 w-35` : `h-24 w-24`}`}
+              />
+            ) : (
+              <LuUser className="h-24 w-24 text-[#555]" />
+            )}
+            <input
+              type="file"
+              ref={fileInputRef}
+              accept="image/*"
+              className="hidden"
+              onChange={handleImageChange}
+            />
+          </label>
+
+          <div className="middle:gap-0 mt-10 flex w-full items-center justify-between gap-8 px-4"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
