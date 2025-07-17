54575d30da494fb8093ea7212f89bdb521724b56
Walker-Gideon <gideonawortwe3@gmail.com>
2025-07-17 02:04:56 +0000

    Added the loader and close button component to the sign up section

--- src/features/auth/Singup.jsx
+++ src/features/auth/Singup.jsx
@@ -1,3 +1,13 @@
+import Loader from "../../ui/Loader";
+import AuthClose from "./AuthClose";
+
 export default function Singup() {
-  return <div>Singup</div>;
+  return (
+    <div className="relative">
+      <div className="h-1">
+        <Loader />
+      </div>
+      <AuthClose />
+    </div>
+  );
 }
