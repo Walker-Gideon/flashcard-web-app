2df768b0f778365f7b2c6b68d8871e938250caff
Walker-Gideon <gideonawortwe3@gmail.com>
2025-07-17 01:43:14 +0000

    Added the forget password and sign up button

--- src/features/auth/Login.jsx
+++ src/features/auth/Login.jsx
@@ -1,11 +1,29 @@
+import { useNavigate } from "react-router-dom";
+import { useLoader } from "../../context/LoaderContext";
+import Button from "../../ui/Button";
 import Loader from "../../ui/Loader";
 import AuthClose from "./AuthClose";
 import AuthHeader from "./AuthHeader";
 import LoginForm from "./LoginForm";
 
 export default function Login() {
+  const { setLoading } = useLoader();
+
+  const navigate = useNavigate();
+  const startLoadingAndNavigate = (to) => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      navigate(to);
+    }, 500);
+  };
+
+  const clascName = {
+    button: "text-[0.8rem] font-semibold",
+  };
+
   return (
-    <div className="relative">
+    <div clacsName="relative">
       <div className="h-1">
         <Loader />
       </div>
@@ -15,6 +33,25 @@ export default function Login() {
         <div className="rounded-xl border border-stone-300 px-6 py-4 shadow-lg">
           <AuthHeader />
           <LoginForm />
+
+          <div className="mt-2 flex items-center justify-between">
+            <Button
+              variant="outline"
+              className={clascName.button}
+              onClick={() => {
+                console.log("forget password");
+              }}
+            >
+              Forget Password?
+            </Button>
+            <Button
+              variant="outline"
+              className={clascName.button}
+              onClick={() => startLoadingAndNavigate("/accounts/signup")}
+            >
+              Sign Up
+            </Button>
+          </div>
         </div>
       </div>
     </div>
