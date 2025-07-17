602316efe64d24989e51037f13d0a92b6cafa3c8
Walker-Gideon <gideonawortwe3@gmail.com>
2025-07-17 04:17:40 +0000

    Added the spinner styling to the css file

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,7 +1,11 @@
 import { useLoader } from "../context/LoaderContext";
 
+//  <div className="absolute inset-0 flex items-center justify-center bg-slate-200/20 backdrop-blur-sm">
+
 export default function Loader() {
-  const { loading } = useLoader();
+  const { loading, spinner } = useLoader();
+
+  if (spinner) return <div className="progress bg-black text-black" />;
 
   return (
     <div>{loading && <div className="progress bg-black text-black" />}</div>
