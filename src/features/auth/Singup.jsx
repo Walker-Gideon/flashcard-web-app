f81c4480bc3c8d65f9ff30dd4266c7d82c8dccd8
Walker-Gideon <gideonawortwe3@gmail.com>
2025-07-17 02:06:25 +0000

    Added the authHeader component to the sign up section

--- src/features/auth/Singup.jsx
+++ src/features/auth/Singup.jsx
@@ -1,5 +1,6 @@
 import Loader from "../../ui/Loader";
 import AuthClose from "./AuthClose";
+import AuthHeader from "./AuthHeader";
 
 export default function Singup() {
   return (
@@ -8,6 +9,12 @@ export default function Singup() {
         <Loader />
       </div>
       <AuthClose />
+
+      <div className="flex min-h-[95vh] items-center justify-center">
+        <div className="rounded-xl border border-stone-300 px-6 py-8 shadow-lg">
+          <AuthHeader />
+        </div>
+      </div>
     </div>
   );
 }
