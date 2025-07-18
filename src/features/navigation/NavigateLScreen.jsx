346a6d6ea3824297861c7cc04569cd1f8225a13f
Walker-Gideon <gideonawortwe3@gmail.com>
2025-07-18 14:00:33 +0000

    Added the navigation buttons data

--- src/features/navigation/NavigateLScreen.jsx
+++ src/features/navigation/NavigateLScreen.jsx
@@ -1,4 +1,42 @@
+import { LuLayoutDashboard } from "react-icons/lu";
+import { LuBookOpen } from "react-icons/lu";
+import { LuFile } from "react-icons/lu";
+import { LuCalendarCheck2 } from "react-icons/lu";
+import { LuLightbulb } from "react-icons/lu";
+import { LuMessageCircle } from "react-icons/lu";
+import { LuSettings2 } from "react-icons/lu";
 import Logo from "../../ui/Logo";
+
+const buttonsData = [
+  {
+    text: "Dashboard",
+    to: "/dashboard",
+    icon: <LuLayoutDashboard />,
+  },
+  {
+    text: "Notes",
+    to: "notes",
+    icon: <LuBookOpen />,
+  },
+  {
+    text: "Flashcards",
+    to: "flashcards",
+    icon: <LuFile />,
+  },
+];
+
+const nextButtonsData = [
+  {
+    text: "Schedules",
+    to: "schedules",
+    icon: <LuCalendarCheck2 />,
+  },
+  {
+    text: "Inspire",
+    to: "inspire",
+    icon: <LuLightbulb />,
+  },
+];
 
 export default function NavigateLScreen() {
   return (
