27955260d958ab122823d6497b0f6fb07eb8012d
Walker-Gideon <gideonawortwe3@gmail.com>
2025-07-18 13:38:27 +0000

    Created the small navigation screen

---
+++ src/features/navigation/NavigateSmallScreen.jsx
@@ -0,0 +1,16 @@
+import Button from "../../ui/Button";
+
+export default function NavigateSmallScreen() {
+  return (
+    <header className="medium:hidden block border-b border-stone-300 px-4 py-5 shadow-sm">
+      <Button
+        variant="outline"
+        className={`flex cursor-pointer flex-col gap-1.5 rounded text-2xl`}
+        onClick={() => console.log("clicked")}
+      >
+        <div className={`h-[3px] w-6 rounded-lg bg-slate-950`}></div>
+        <div className={`h-[2.5px] w-4 rounded-lg bg-slate-950`}></div>
+      </Button>
+    </header>
+  );
+}
