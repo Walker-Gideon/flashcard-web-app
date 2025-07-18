3fe258d7c76ef40acfe4da067e9a8a0760e2aa53
Walker-Gideon <gideonawortwe3@gmail.com>
2025-07-18 13:24:18 +0000

    Added the phone menu button in the navigation

--- src/AppLayout.jsx
+++ src/AppLayout.jsx
@@ -3,7 +3,8 @@ import NavigationLayout from "./features/navigation/NavigationLayout";
 
 export default function AppLayout() {
   return (
-    <div>
+    // medium:grid medium:grid-cols-[auto_1fr] h-screen
+    <div className={`medium:grid medium:grid-cols-[auto_1fr] relative`}>
       <NavigationLayout />
       <Outlet />
     </div>
--- src/features/navigation/NavigationLayout.jsx
+++ src/features/navigation/NavigationLayout.jsx
@@ -1,3 +1,17 @@
+import Button from "../../ui/Button";
+
 export default function NavigationLayout() {
-  return <div>NavigationLayout</div>;
+  return (
+    <div>
+      <div className="">
+        <Button
+          variant="outline"
+          className={`flex cursor-pointer flex-col gap-1.5 rounded text-2xl`}
+        >
+          <div className={`h-[3px] w-7 rounded-lg bg-stone-300`}></div>
+          <div className={`h-[2.5px] w-5 rounded-lg bg-stone-300`}></div>
+        </Button>
+      </div>
+    </div>
+  );
 }
