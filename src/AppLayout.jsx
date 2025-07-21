d767826264a4e73e8cdb998666c1f1c44e88e600
Walker-Gideon <gideonawortwe3@gmail.com>
2025-07-21 03:30:52 +0000

    Added the overlay component to the appLayout component

--- src/AppLayout.jsx
+++ src/AppLayout.jsx
@@ -1,11 +1,16 @@
 import { Outlet } from "react-router-dom";
 import NavigationLayout from "./features/navigation/NavigationLayout";
+import Overlay from "./ui/Overlay";
 
 export default function AppLayout() {
   return (
     <div className={`medium:grid medium:grid-cols-[auto_1fr] relative`}>
       <NavigationLayout />
-      <Outlet />
+
+      <div className="">
+        <Outlet />
+        <Overlay />
+      </div>
     </div>
   );
 }
