5f9a1f04b0a141937cd51c27b4da59e69c28001f
Walker-Gideon <gideonawortwe3@gmail.com>
2025-07-21 04:03:52 +0000

    Allow the overlay to show and reomve by the navgate button

--- src/AppLayout.jsx
+++ src/AppLayout.jsx
@@ -1,16 +1,17 @@
 import { Outlet } from "react-router-dom";
 import NavigationLayout from "./features/navigation/NavigationLayout";
 import Overlay from "./ui/Overlay";
+import { useNav } from "./context/NavigateContext";
 
 export default function AppLayout() {
+  const { navShowOverLay } = useNav();
+
   return (
     <div className={`medium:grid medium:grid-cols-[auto_1fr] relative`}>
       <NavigationLayout />
+      <Outlet />
 
-      <div className="">
-        <Outlet />
-        <Overlay />
-      </div>
+      <div className="medium:hidden block">{navShowOverLay && <Overlay />}</div>
     </div>
   );
 }
--- src/features/navigation/NavigateSmallScreen.jsx
+++ src/features/navigation/NavigateSmallScreen.jsx
@@ -1,12 +1,19 @@
+import { useNav } from "../../context/NavigateContext";
 import Button from "../../ui/Button";
 
 export default function NavigateSmallScreen() {
+  const { setNavShowOverLay } = useNav();
+
+  function handleClick() {
+    setNavShowOverLay(true);
+  }
+
   return (
     <header className="medium:hidden block border-b border-stone-300 px-4 py-5 shadow-sm">
       <Button
         variant="outline"
         className={`flex cursor-pointer flex-col gap-1.5 rounded text-2xl`}
-        onClick={() => console.log("clicked")}
+        onClick={handleClick}
       >
         <div className={`h-[3px] w-6 rounded-lg bg-slate-950`}></div>
         <div className={`h-[2.5px] w-4 rounded-lg bg-slate-950`}></div>
