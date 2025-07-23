a9a00563ea4d9f353a35d9f3b5cfa5b92ab07513
Walker-Gideon <gideonawortwe3@gmail.com>
2025-07-23 11:10:54 +0000

    Created the layout and styling for the mainContent and pass the the mainContent Text and Image component to it

--- src/features/home/mainoverview/MainContent.jsx
+++ src/features/home/mainoverview/MainContent.jsx
@@ -1,3 +1,28 @@
-export default function MainContent() {
-  return <div></div>;
+import MainContentImage from "./MainContentImage";
+import MainContentText from "./MainContentText";
+
+export default function MainContent({
+  index,
+  icon,
+  title,
+  subtitle,
+  points,
+  image,
+}) {
+  return (
+    <div className="px-2 py-4 md:px-6 md:py-0 lg:px-15">
+      <div
+        className={`flex flex-col-reverse gap-8 md:grid md:h-100 md:grid-cols-2 md:items-center lg:gap-20`}
+      >
+        <MainContentText
+          index={index}
+          icon={icon}
+          title={title}
+          subtitle={subtitle}
+          points={points}
+        />
+        <MainContentImage image={image} index={index} />
+      </div>
+    </div>
+  );
 }
