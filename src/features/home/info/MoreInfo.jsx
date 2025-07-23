19950ca672ce2b359a0e574b356a245e20917bf3
Walker-Gideon <gideonawortwe3@gmail.com>
2025-07-23 11:22:33 +0000

    Created the moreInfo layout and pass it inner components

--- src/features/home/info/MoreInfo.jsx
+++ src/features/home/info/MoreInfo.jsx
@@ -1,3 +1,11 @@
+import InfoContent from "./InfoContent";
+import InfoHeader from "./InfoHeader";
+
 export default function MoreInfo() {
-  return <div>MoreInfo</div>;
+  return (
+    <div className="middle:grid middle:grid-cols-3 middle:items-center middle:gap-10 bg-stone-400/30 p-8 md:p-14 lg:px-35 lg:py-20">
+      <InfoHeader />
+      <InfoContent />
+    </div>
+  );
 }
