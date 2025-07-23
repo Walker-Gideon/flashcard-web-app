dab36e247cf0b4444056fd81a6735ebd925e0df7
Walker-Gideon <gideonawortwe3@gmail.com>
2025-07-23 08:31:18 +0000

    Pass the Hero, Mainoverview, Moreinfo, Testimonial, and Footer to the landing Page

--- src/features/home/LandingPage.jsx
+++ src/features/home/LandingPage.jsx
@@ -1,5 +1,10 @@
+import Footer from "../../ui/Footer";
 import Loader from "../../ui/Loader";
+import Hero from "./Hero";
 import HomeHeader from "./HomeHeader";
+import MainOverview from "./MainOverview";
+import MoreInfo from "./MoreInfo";
+import Testimonial from "./Testimonial";
 
 export default function LandingPage() {
   return (
@@ -8,6 +13,15 @@ export default function LandingPage() {
         <Loader />
       </div>
       <HomeHeader />
+
+      <main>
+        <Hero />
+        <MainOverview />
+        <MoreInfo />
+        <Testimonial />
+      </main>
+
+      <Footer />
     </div>
   );
 }
