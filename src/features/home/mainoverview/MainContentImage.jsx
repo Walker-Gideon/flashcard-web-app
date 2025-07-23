4b72c9d4a8e89e0972a7c23bc572899013a75e27
Walker-Gideon <gideonawortwe3@gmail.com>
2025-07-23 11:06:13 +0000

    Set the layout and styling for the mainContent image component

--- src/features/home/mainoverview/MainContentImage.jsx
+++ src/features/home/mainoverview/MainContentImage.jsx
@@ -1,3 +1,10 @@
-export default function MainContentImage() {
-  return <div></div>;
+export default function MainContentImage({ image, index }) {
+  return (
+    <div
+      className={`${index === 1 ? `md:order-1` : ``} ${index === 3 ? `md:order-1` : ``}`}
+    >
+      {/* <img src={image} alt="image" /> */}
+      <div className="h-60 w-full rounded-2xl bg-black"></div>
+    </div>
+  );
 }
