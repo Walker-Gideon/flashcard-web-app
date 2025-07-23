c3d735b66a21597981b7581a012c17833277cf4f
Walker-Gideon <gideonawortwe3@gmail.com>
2025-07-23 11:20:47 +0000

    Created the infoHeader layout and pass it data

--- src/features/home/info/InfoHeader.jsx
+++ src/features/home/info/InfoHeader.jsx
@@ -1,3 +1,16 @@
 export default function InfoHeader() {
-  return <div></div>;
+  return (
+    <header className="middle:col-span-2 mb-8 text-left font-medium">
+      <h1 className="middle:text-6xl text-4xl font-bold">Why WalkWise?</h1>
+
+      <p className="py-1 text-sm md:text-base">
+        Forget last-minute cramming — learn the smarter way.
+      </p>
+
+      <p className="text-xl font-semibold md:text-2xl">
+        WalkWise isn’t just another study app. It’s your all-in-one learning
+        assistant, built for real students facing real challenges.
+      </p>
+    </header>
+  );
 }
