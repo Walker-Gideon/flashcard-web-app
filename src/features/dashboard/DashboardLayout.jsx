24d19645aa3edf8e26b52d7cdc84c93be1cb5cbc
Walker-Gideon <gideonawortwe3@gmail.com>
2025-07-24 12:49:00 +0000

    Set the dark and light them in the nav and dashboard section

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,6 +1,6 @@
 export default function DashboardLayout() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-white text-black transition-colors duration-500 dark:bg-zinc-900 dark:text-white">
+    <div className="defaultColor flex min-h-screen flex-col items-center justify-center text-black">
       <h2>DashboardLayout</h2>
       <h1 className="mb-4 text-3xl font-bold">Hello Walkwise 👋</h1>
     </div>
