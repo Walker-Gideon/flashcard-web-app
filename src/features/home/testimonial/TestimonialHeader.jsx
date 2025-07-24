da2a94afe5b5bf47bc9a5b3d8fb3027d5b6cb022
Walker-Gideon <gideonawortwe3@gmail.com>
2025-07-24 04:44:55 +0000

    Created the TestimonailHeader layout

--- src/features/home/testimonial/TestimonialHeader.jsx
+++ src/features/home/testimonial/TestimonialHeader.jsx
@@ -1,3 +1,7 @@
 export default function TestimonialHeader() {
-  return <div></div>;
+  return (
+    <header className="mb-6 text-center text-xl font-bold md:text-3xl">
+      <h1>What Students Are Saying</h1>
+    </header>
+  );
 }
