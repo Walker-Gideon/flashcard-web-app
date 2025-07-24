96b0d8fcd1a1564596735a102ee7947c2d2ccc44
Walker-Gideon <gideonawortwe3@gmail.com>
2025-07-24 05:52:57 +0000

    Pass footer compoents to the footer section

--- src/ui/footer/Footer.jsx
+++ src/ui/footer/Footer.jsx
@@ -1,3 +1,19 @@
+import Logo from "../Logo";
+import FooterLinks from "./FooterLinks";
+import FooterRight from "./FooterRight";
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="middle:px-8 px-4 pb-6 md:px-8 lg:px-30">
+      <div className="medium:flex-row medium:pt-4 medium:pb-20 flex flex-col justify-between gap-4 border-t border-stone-300 py-8">
+        <div className="flex items-center gap-1 font-bold text-slate-950 uppercase">
+          <Logo logo={true} />
+          <span className={`hidden font-bold md:block`}>walkwise</span>
+        </div>
+        <FooterLinks />
+      </div>
+
+      <FooterRight />
+    </footer>
+  );
 }
