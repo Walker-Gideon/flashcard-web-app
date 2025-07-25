b8f08b710c2eec2d66c087073016cfbea0802be5
Walker-Gideon <gideonawortwe3@gmail.com>
2025-07-25 22:16:40 +0000

    Created the left and right note compnent

--- src/features/note/NoteLayout.jsx
+++ src/features/note/NoteLayout.jsx
@@ -1,8 +1,11 @@
+import NoteLeftLayout from "./noteLeft/NoteLeftLayout";
+import NoteRightLayout from "./noteRight/NoteRightLayout";
+
 export default function NoteLayout() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-white text-black transition-colors duration-500 dark:bg-zinc-900 dark:text-white">
-      <h2>NoteLayout</h2>
-      <h1 className="mb-4 text-3xl font-bold">Hello Walkwise 👋</h1>
+    <div className="min-h-screen">
+      <NoteLeftLayout />
+      <NoteRightLayout />
     </div>
   );
 }
