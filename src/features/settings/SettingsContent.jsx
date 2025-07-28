379630c6ecedd363caf2a5034cffd07b76c46895
Walker-Gideon <gideonawortwe3@gmail.com>
2025-07-28 22:06:28 +0000

    created the settings content header

--- src/features/settings/SettingsContent.jsx
+++ src/features/settings/SettingsContent.jsx
@@ -1,9 +1,12 @@
 import CardOverview from "../../ui/CardOverview";
+import SettlingsFormHeader from "./SettlingsFormHeader";
 
 export default function SettingsContent() {
   return (
     <div className="medium:w-140 medium:mt-8 mt-0 w-auto lg:w-160">
-      <CardOverview></CardOverview>
+      <CardOverview>
+        <SettlingsFormHeader />
+      </CardOverview>
     </div>
   );
 }
---
+++ src/features/settings/SettlingsFormHeader.jsx
@@ -0,0 +1,12 @@
+export default function SettlingsFormHeader() {
+  return (
+    <header>
+      <h1 className="text-base font-bold text-slate-900 dark:text-white">
+        Profile Information
+      </h1>
+      <p className="text-sm text-slate-500 dark:text-slate-400">
+        Update your profile information visible to other users
+      </p>
+    </header>
+  );
+}
