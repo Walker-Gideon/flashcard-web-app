7019ce676c8cbf4349386772ca05b9c151b9eb53
Walker-Gideon <gideonawortwe3@gmail.com>
2025-08-01 01:12:06 +0000

    Refactor the codes in the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,131 +1,25 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Routes,
-  Route,
-} from "react-router-dom";
-import { LoaderProvider } from "./context/LoaderContext";
+import { Routes, Route } from "react-router-dom";
 import LandingPage from "./features/home/LandingPage";
 import AuthLayout from "./features/auth/AuthLayout";
 import Login from "./features/auth/Login";
-import { loginAction } from "./services/actions/loginAction";
 import Singup from "./features/auth/Singup";
-import { signUpAction } from "./services/actions/signUpAction";
 import ForgetAuthPassword from "./features/auth/ForgetAuthPassword";
 import AppLayout from "./AppLayout";
 import DashboardLayout from "./features/dashboard/DashboardLayout";
 import NoteLayout from "./features/note/NoteLayout";
-import { noteAction } from "./services/actions/noteAction";
 import FlashcardLayout from "./features/flashcard/FlashcardLayout";
 import SchedulesLayout from "./features/schedules/SchedulesLayout";
 import InspireLayout from "./features/inspire/InspireLayout";
 import SettingsLayout from "./features/settings/SettingsLayout";
-import { NavigateProvider } from "./context/NavigateContext";
 import ProtectedRoute from "./utils/ProtectedRoute";
-import { AuthProvider, useAuth } from "./context/AuthContext";
-import { NoteProvider } from "./context/NoteContext";
 import Verify from "./ui/Verify";
-import { forgetPasswordAction } from "./services/actions/forgetPasswordAction";
 import AuthRedirectRoute from "./utils/AuthRedirectRoute";
-
-/*
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <LandingPage />,
-  },
-  {
-    element: <AuthRedirectRoute />,
-    children: [
-      {
-        path: "/accounts",
-        element: <AuthLayout />,
-        children: [
-          {
-            path: "login",
-            element: <Login />,
-            action: loginAction,
-          },
-          {
-            path: "signup",
-            element: <Singup />,
-            action: signUpAction,
-          },
-          {
-            path: "forgotten",
-            element: <ForgetAuthPassword />,
-            action: forgetPasswordAction,
-          },
-        ],
-      },
-    ],
-  },
-  {
-    // ProtectedRoute
-    element: <AuthRedirectRoute />,
-    children: [
-      {
-        path: "verify",
-        element: <Verify />,
-      },
-      {
-        path: "dashboard",
-        //(
-        element: (
-          // <AuthRedirectRoute>
-          <AppLayout />
-        ),
-        // </AuthRedirectRoute>
-        // ),
-        children: [
-          {
-            index: true,
-            element: <DashboardLayout />,
-          },
-          {
-            path: "notes",
-            element: <NoteLayout />,
-            action: noteAction,
-          },
-          {
-            path: "flashcards",
-            element: <FlashcardLayout />,
-          },
-          {
-            path: "schedules",
-            element: <SchedulesLayout />,
-          },
-          {
-            path: "inspire",
-            element: <InspireLayout />,
-          },
-          {
-            path: "settings",
-            element: <SettingsLayout />,
-          },
-        ],
-      },
-    ],
-  },
-]);
-*/
+import { useAuth } from "./context/AuthContext";
 
 export default function App() {
-  // let isAuthenticated = false;
   const { isAuthenticated } = useAuth();
 
   return (
-    /*
-    <NoteProvider>
-      <NavigateProvider>
-        <AuthProvider>
-          <LoaderProvider>
-            <RouterProvider router={router} />
-          </LoaderProvider>
-        </AuthProvider>
-      </NavigateProvider>
-    </NoteProvider>
-    */
     <Routes>
       <Route path="/" element={<LandingPage />} />
 
@@ -142,10 +36,8 @@ export default function App() {
         <Route path="forgotten" element={<ForgetAuthPassword />} />
       </Route>
 
-      {/* Protected Route */}
       <Route path="/verify" element={<Verify />} />
 
-      {/* Dashboard routes */}
       <Route
         path="/dashboard"
         element={
@@ -164,10 +56,3 @@ export default function App() {
     </Routes>
   );
 }
-
-/*
-In your user display components
-Will use this later
-const displayName = user.displayName || user.email.split('@')[0];
-const avatarUrl = user.photoURL || '/default-avatar.png'; // or use initials
-*/
