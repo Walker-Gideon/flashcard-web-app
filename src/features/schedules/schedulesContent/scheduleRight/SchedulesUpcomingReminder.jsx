7136cb7470c9aa1b13a8253503e5d4252d1af46a
Walker-Gideon <gideonawortwe3@gmail.com>
2025-08-02 23:24:04 +0000

    Finish with the UpcomingReminder section

--- src/features/schedules/schedulesContent/scheduleRight/SchedulesUpcomingReminder.jsx
+++ src/features/schedules/schedulesContent/scheduleRight/SchedulesUpcomingReminder.jsx
@@ -1,22 +1,28 @@
 import { LuClock } from "react-icons/lu";
+import CardOverview from "../../../../ui/CardOverview";
+import Button from "../../../../ui/Button";
 
 export default function SchedulesUpcomingReminder() {
   return (
-    // lg:col-span-2
-    <div className="medium:col-span-2 mb-18 rounded-2xl bg-gradient-to-br from-emerald-500 to-teal-600 p-6 text-white lg:col-span-1">
+    <CardOverview classname="medium:col-span-2 mb-18 rounded-2xl dark:text-white lg:col-span-1 bg-slate-500">
       <div className="mb-3 flex items-center space-x-2">
-        <LuClock className="h-5 w-5 text-emerald-200" />
+        <LuClock className="h-5 w-5" />
         <h3 className="font-semibold">Next Session</h3>
       </div>
-      <p className="mb-4 text-emerald-50">Biology review starts in 2 hours</p>
+
+      {/* This data will come for the user */}
+      <p className="mb-4 font-medium">Biology review starts in 2 hours</p>
+
       <div className="flex items-center justify-between">
         <div>
-          <p className="text-sm text-emerald-100">12 cards • 25 min</p>
+          {/* This data will come for the user */}
+          <p className="text-sm">12 cards • 25 min</p>
         </div>
-        <button className="rounded-lg bg-white/20 px-4 py-2 text-sm font-medium transition-colors hover:bg-white/30">
+
+        <Button variant="outline" classname="primaryButton">
           Reschedule
-        </button>
+        </Button>
       </div>
-    </div>
+    </CardOverview>
   );
 }
