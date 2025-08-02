5d9a2c712440d4368ece419ed31b3027a57d22bf
Walker-Gideon <gideonawortwe3@gmail.com>
2025-08-02 01:58:28 +0000

    Adjustment in the ScheduleLayout styling

--- src/features/schedules/SchedulesLayout.jsx
+++ src/features/schedules/SchedulesLayout.jsx
@@ -8,11 +8,10 @@ export default function SchedulesLayout() {
   // const [selectedDate, setSelectedDate] = useState(new Date());
 
   return (
-    // min-h-screen overflow-hidden bg-gradient-to-br from-slate-50 via-white to-slate-100 p-6 dark:from-slate-900 dark:via-slate-800 dark:to-slate-900
     <div className="defaultColor h-screen overflow-hidden">
       <SchedulesHeader activeView={activeView} setActiveView={setActiveView} />
 
-      <div className="h-screen overflow-scroll">
+      <div className="medium:mt-0 mt-7 h-screen space-y-6 overflow-scroll p-6">
         <SchedulesOverview />
         <SchedulesMainContent activeView={activeView} />
       </div>
