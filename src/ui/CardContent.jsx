5f618b23019ffa6cc1704bafa446d09968e512ee
Walker-Gideon <gideonawortwe3@gmail.com>
2025-08-02 03:14:36 +0000

    Adjustment to the CardContent prop

--- src/ui/CardContent.jsx
+++ src/ui/CardContent.jsx
@@ -1,11 +1,22 @@
-export default function CardContent({ children, classname, type }) {
+export default function CardContent({
+  children,
+  classname,
+  type,
+  role,
+  onClick,
+}) {
   const styling = {
     default: `${classname ? `${classname}` : `flex items-center justify-between`}`,
     innerCard:
       "flex h-10 w-10 items-center justify-center rounded-lg bg-gradient-to-r from-slate-200 to-slate-300 dark:from-slate-600 dark:to-slate-700",
   };
   return (
-    <div className={`${classname ? `${styling.default}` : `${styling[type]}`}`}>
+    <div
+      // "button"
+      role={role}
+      className={`${classname ? `${styling.default}` : `${styling[type]}`}`}
+      onClick={onClick}
+    >
       {children}
     </div>
   );
