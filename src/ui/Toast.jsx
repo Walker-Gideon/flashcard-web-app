f2b52cf18924bba4b0b6abc6bd1bce804eeda166
Walker-Gideon <gideonawortwe3@gmail.com>
2025-08-03 14:47:01 +0000

    Update the toast component with a displaying other content

--- src/ui/Toast.jsx
+++ src/ui/Toast.jsx
@@ -1,6 +1,29 @@
 import { motion } from "motion/react";
+import Overlay from "./Overlay";
 
-export default function Toast({ children, classname }) {
+export default function Toast({ children, classname, model, type }) {
+  const styling = {
+    main: "fixed top-5 left-1/2 z-50 -translate-x-1/2 rounded-sm text-sm text-white shadow-lg",
+  };
+  //"bg-green-600" : "bg-red-600"
+
+  if (model)
+    return (
+      <Overlay model={true}>
+        <motion.div
+          initial={{ y: "-400%" }}
+          transition={{
+            duration: 0.6,
+            ease: "easeInOut",
+          }}
+          animate={{ y: "0" }}
+          className={`p-2 ${styling[type]} ${classname}`}
+        >
+          {children}
+        </motion.div>
+      </Overlay>
+    );
+
   return (
     <div className="absolute inset-0 flex items-center justify-center bg-slate-200/20 backdrop-blur-sm">
       <motion.div
@@ -10,8 +33,7 @@ export default function Toast({ children, classname }) {
           ease: "easeInOut",
         }}
         animate={{ y: "0" }}
-        // bg-green-600
-        className={`fixed top-5 left-1/2 z-50 -translate-x-1/2 rounded-sm px-6 py-2 text-sm text-white shadow-lg ${classname}`}
+        className={`px-6 py-2 ${styling[type]} ${classname}`}
       >
         {children}
       </motion.div>
