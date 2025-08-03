daceb2a86b1a1aab292eceeacbe6c83f2f5b2bfb
Walker-Gideon <gideonawortwe3@gmail.com>
2025-08-03 23:52:41 +0000

    Working on the DisplayNote layout

--- src/features/note/noteLeft/DisplayNoteCreated.jsx
+++ src/features/note/noteLeft/DisplayNoteCreated.jsx
@@ -1,3 +1,16 @@
 export default function DisplayNoteCreated() {
-  return <div>DisplayNoteCreated</div>;
+  return (
+    <div className="">
+      <div
+        role="button"
+        className="my-1 flex w-full cursor-pointer items-center justify-between bg-amber-400 px-4 py-2"
+      >
+        <h1 className="medium:text-sm">Title</h1>
+
+        <div className="medium:text-xs text-sm">
+          <span>X</span> <span>days ago</span>
+        </div>
+      </div>
+    </div>
+  );
 }
