4fedc5920dc0d42a929902f89fe8b622f574b1b6
Walker-Gideon <gideonawortwe3@gmail.com>
2025-08-04 02:43:38 +0000

    Set the layout for the DispplayNoteCreated

--- src/features/note/noteLeft/DisplayNoteCreated.jsx
+++ src/features/note/noteLeft/DisplayNoteCreated.jsx
@@ -3,13 +3,15 @@ export default function DisplayNoteCreated() {
     <div className="">
       <div
         role="button"
-        className="my-1 flex w-full cursor-pointer items-center justify-between bg-amber-400 px-4 py-2"
+        className="my-1 w-full cursor-pointer px-4 py-2 transition-colors hover:bg-slate-50 dark:hover:bg-slate-700"
       >
-        <h1 className="medium:text-sm">Title</h1>
+        <h1 className="medium:text-sm mb-1 text-slate-900 dark:text-white">
+          Title
+        </h1>
 
-        <div className="medium:text-xs text-sm">
+        <p className="medium:text-xs text-sm text-slate-500 dark:text-slate-400">
           <span>X</span> <span>days ago</span>
-        </div>
+        </p>
       </div>
     </div>
   );
