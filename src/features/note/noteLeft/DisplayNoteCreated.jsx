071eb9b4bc3dcb8c486c7046e4e99627932f1ed4
Walker-Gideon <gideonawortwe3@gmail.com>
2025-08-04 02:47:06 +0000

    Created the component for the display

--- src/features/note/noteLeft/DisplayNoteCreated.jsx
+++ src/features/note/noteLeft/DisplayNoteCreated.jsx
@@ -1,18 +1,9 @@
+import DisplayCreated from "../../../ui/DisplayCreated";
+
 export default function DisplayNoteCreated() {
   return (
     <div className="">
-      <div
-        role="button"
-        className="my-1 w-full cursor-pointer px-4 py-2 transition-colors hover:bg-slate-50 dark:hover:bg-slate-700"
-      >
-        <h1 className="medium:text-sm mb-1 text-slate-900 dark:text-white">
-          Title
-        </h1>
-
-        <p className="medium:text-xs text-sm text-slate-500 dark:text-slate-400">
-          <span>X</span> <span>days ago</span>
-        </p>
-      </div>
+      <DisplayCreated />
     </div>
   );
 }
