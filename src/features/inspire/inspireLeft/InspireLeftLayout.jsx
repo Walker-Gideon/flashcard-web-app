0353c74b0ba98794a35a92c5da9adf5e197d4576
Walker-Gideon <gideonawortwe3@gmail.com>
2025-08-04 21:47:43 +0000

    Created the inspireLeft layout and it's sub components

---
+++ src/features/inspire/inspireLeft/InspireLeftLayout.jsx
@@ -0,0 +1,13 @@
+import Achievement from "./Achievement";
+import Motivation from "./Motivation";
+import Progress from "./Progress";
+
+export default function InspireLeftLayout() {
+  return (
+    <div>
+      <Motivation />
+      <Achievement />
+      <Progress />
+    </div>
+  );
+}
