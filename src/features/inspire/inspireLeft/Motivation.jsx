f7e5b1da54c2aa0b7015565b1b9e3cd976b0628b
Walker-Gideon <gideonawortwe3@gmail.com>
2025-08-04 22:06:12 +0000

    Adjust the styling in the motivation section

--- src/features/inspire/inspireLeft/Motivation.jsx
+++ src/features/inspire/inspireLeft/Motivation.jsx
@@ -1,19 +1,18 @@
+import CardOverview from "../../../ui/CardOverview";
 import { LuMoon } from "react-icons/lu";
 
 export default function Motivation({ currentQuote }) {
   return (
-    <div className="rounded-2xl bg-gradient-to-br from-emerald-500 to-teal-600 p-6 text-white shadow-lg">
+    <CardOverview classname={"text-slate-900 dark:text-white"}>
       <div className="mb-4 flex items-center space-x-3">
         {/* Lightbulb */}
-        <LuMoon className="h-6 w-6 text-emerald-200" />
+        <LuMoon className="h-6 w-6 text-slate-600 dark:text-slate-300" />
         <h3 className="text-xl font-semibold">Daily Inspiration</h3>
       </div>
-      <blockquote className="mb-4 text-lg text-emerald-50 italic">
+      <blockquote className="mb-4 text-lg text-slate-500 italic dark:text-slate-400">
         "{currentQuote.text}"
       </blockquote>
-      <p className="text-right text-sm text-emerald-100">
-        - {currentQuote.author}
-      </p>
-    </div>
+      <p className="text-right text-sm">- {currentQuote.author}</p>
+    </CardOverview>
   );
 }
