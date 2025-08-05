dfba2fee4858809a6a7528af6f4c5a5722d825b5
Walker-Gideon <gideonawortwe3@gmail.com>
2025-08-05 04:28:05 +0000

    Chnages in the userLoader context

--- src/context/LoaderContext.jsx
+++ src/context/LoaderContext.jsx
@@ -6,20 +6,17 @@ function LoaderProvider({ children }) {
   const [loading, setLoading] = useState(false);
   const [spinner, setSpinner] = useState(false);
   const [toast, setToast] = useState(false);
-  const [image, setImage] = useState(null);
 
   const value = useMemo(
     () => ({
       loading,
       setLoading,
-      image,
-      setImage,
       spinner,
       setSpinner,
       toast,
       setToast,
     }),
-    [loading, image, spinner, toast],
+    [loading, spinner, toast],
   );
 
   return (
