1e118dcfccb2fd26a6ac5ab9915bfca39ffa9d14
Walker-Gideon <gideonawortwe3@gmail.com>
2025-08-05 17:50:02 +0000

    Adjust the motivation section sub heading

--- src/features/inspire/inspireLeft/Motivation.jsx
+++ src/features/inspire/inspireLeft/Motivation.jsx
@@ -1,12 +1,13 @@
 import CardOverview from "../../../ui/CardOverview";
 import { LuLightbulb } from "react-icons/lu";
+import HeaderText from "../../../ui/HeaderText";
 
 export default function Motivation({ currentQuote }) {
   return (
     <CardOverview classname={"text-slate-900 dark:text-white"}>
       <div className="mb-4 flex items-center space-x-3">
         <LuLightbulb className="h-6 w-6 text-slate-600 dark:text-slate-300" />
-        <h3 className="text-xl font-semibold">Daily Inspiration</h3>
+        <HeaderText>Daily Inspiration</HeaderText>
       </div>
       <blockquote className="mb-4 text-lg text-slate-500 italic dark:text-slate-400">
         "{currentQuote.text}"
