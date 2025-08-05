3d833b89cdfd904da00f7a62a275a821f14ffb55
Walker-Gideon <gideonawortwe3@gmail.com>
2025-08-05 21:33:30 +0000

    Change the reange color in the overview section

--- src/features/dashboard/dashContent/TargetCardStatus.jsx
+++ src/features/dashboard/dashContent/TargetCardStatus.jsx
@@ -9,7 +9,7 @@ export default function TargetCardStatus({ dashboard }) {
   return (
     <div className="h-2 w-full rounded-full bg-slate-200 dark:bg-slate-700">
       <div
-        className="h-2 rounded-full bg-gradient-to-r from-emerald-500 to-teal-600 transition-all duration-500"
+        className="h-2 rounded-full bg-slate-500 transition-all duration-500"
         style={{ width: `${dashboard}` }}
       ></div>
     </div>
