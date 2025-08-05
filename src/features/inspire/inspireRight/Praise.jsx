1528805abd8420bd9c843e00bc29dba3bb1590bb
Walker-Gideon <gideonawortwe3@gmail.com>
2025-08-05 17:57:20 +0000

    Adjust the praise section

--- src/features/inspire/inspireRight/Praise.jsx
+++ src/features/inspire/inspireRight/Praise.jsx
@@ -1,13 +1,12 @@
 import { LuAward } from "react-icons/lu";
+import HeaderText from "../../../ui/HeaderText";
 
 export default function Praise({ currentPraise }) {
   return (
     <div className="rounded-2xl bg-gradient-to-r from-slate-200 to-slate-300 p-6 shadow-lg dark:from-slate-600 dark:to-slate-700">
       <div className="mb-4 flex items-center space-x-3">
         <LuAward className="h-6 w-6 text-slate-600 dark:text-slate-300" />
-        <h3 className="text-xl font-semibold text-slate-900 dark:text-white">
-          Your Encouragement
-        </h3>
+        <HeaderText>Your Encouragement</HeaderText>
       </div>
       <p className="mb-4 text-lg text-slate-500 italic dark:text-slate-400">
         "{currentPraise}"
