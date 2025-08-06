78b11b9fdf7f8c1ff8ac1c7c923268cd74c8a27a
Walker-Gideon <gideonawortwe3@gmail.com>
2025-08-06 22:39:22 +0000

    Change the format of display for the LOader componenet

--- src/AppLayout.jsx
+++ src/AppLayout.jsx
@@ -9,26 +9,29 @@ export default function AppLayout() {
   const { navShowOverLay } = useNav();
 
   return (
-    <div className="defaultColor h-screen overflow-hidden">
-      <div className="medium:h-[0.5px] h-0">
+    <div className="defaultColor">
+      <div className="medium:h-0.5 z-50 h-0">
         <Loader />
       </div>
 
-      <div
-        className={`medium:grid medium:grid-cols-[auto_1fr] overflow-hidden`}
-      >
-        <NavigationLayout />
+      {/*  */}
+      <div className="defaultColor z-30 h-[99.9vh] w-full overflow-hidden">
+        <div
+          className={`medium:grid medium:grid-cols-[auto_1fr] overflow-hidden`}
+        >
+          <NavigationLayout />
 
-        <div className={`h-screen w-full`}>
-          <Outlet />
+          <div className={`h-screen w-full`}>
+            <Outlet />
 
-          <div className="medium:hidden block transition-all duration-500 ease-in-out">
-            {navShowOverLay && <Overlay index="z-30" btn={true} />}
+            <div className="medium:hidden block transition-all duration-500 ease-in-out">
+              {navShowOverLay && <Overlay index="z-30" btn={true} />}
+            </div>
           </div>
         </div>
-      </div>
 
-      {/* <ChatLayout /> */}
+        {/* <ChatLayout /> */}
+      </div>
     </div>
   );
 }
