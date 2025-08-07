b31455c8ed15c4b3477b1ef996824e572eb165e7
Walker-Gideon <gideonawortwe3@gmail.com>
2025-08-07 22:36:44 +0000

    Adjust the styling to show the buttons

--- src/features/flashcard/myCreated/CreateBtn.jsx
+++ src/features/flashcard/myCreated/CreateBtn.jsx
@@ -1,6 +1,16 @@
-export default function CreateBtn() {
+import Button from "../../../ui/Button";
+
+export default function CreateBtn({ handleBackToEdit, classname }) {
+  const styling = {
+    smalleOverView:
+      "absolute top-12 right-0 flex w-50 flex-col items-start gap-2 rounded-2xl border border-stone-300 bg-white/70 p-2 text-[0.8rem] text-slate-900 backdrop-blur-xl transition-all duration-300 dark:border-slate-700 dark:bg-slate-800/70 dark:text-white",
+    buttonSmall:
+      "w-full text-start px-4 hover:bg-slate-600 transition-colors duration-300 py-2 transition-colors duration-500 hover:rounded-sm",
+    buttonMedium: "medium:bg-slate-500 medium:rounded-sm",
+  };
+
   return (
-    <>
+    <div className={`${classname}`}>
       <Button
         variant="outline"
         //   onClick={handleBackToEdit}
@@ -18,6 +28,6 @@ export default function CreateBtn() {
       >
         Back to Edit
       </Button>
-    </>
+    </div>
   );
 }
--- src/features/flashcard/myCreated/CreatedHeader.jsx
+++ src/features/flashcard/myCreated/CreatedHeader.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import { AnimatePresence, motion } from "motion/react";
-import Button from "../../../ui/Button";
-import HeaderText from "../../../ui/HeaderText";
 import { LuEllipsis } from "react-icons/lu";
+import HeaderText from "../../../ui/HeaderText";
+import CreateBtn from "./CreateBtn";
 
 export default function CreatedHeader({ handleBackToEdit, tags }) {
   const [isVisible, setIsVisible] = useState(false);
@@ -10,9 +10,6 @@ export default function CreatedHeader({ handleBackToEdit, tags }) {
   const styling = {
     smalleOverView:
       "absolute top-12 right-0 flex w-50 flex-col items-start gap-2 rounded-2xl border border-stone-300 bg-white/70 p-2 text-[0.8rem] text-slate-900 backdrop-blur-xl transition-all duration-300 dark:border-slate-700 dark:bg-slate-800/70 dark:text-white",
-    buttonSmall:
-      "w-full text-start px-4 hover:bg-slate-600 transition-colors duration-300 py-2 transition-colors duration-500 hover:rounded-sm",
-    buttonMedium: "medium:bg-slate-500 medium:rounded-sm",
   };
 
   return (
@@ -28,16 +25,24 @@ export default function CreatedHeader({ handleBackToEdit, tags }) {
           <LuEllipsis className="h-5 w-5 dark:text-white" />
         </motion.button>
 
+        {/* This is for the small screen */}
         <AnimatePresence initial={false}>
           {isVisible ? (
             <motion.div
               initial={{ opacity: 0, scale: 0 }}
               animate={{ opacity: 1, scale: 1 }}
               exit={{ opacity: 0, scale: 0 }}
-              className={`${styling.smalleOverView} medium:relative medium:top-0 medium:border-0 medium:bg-transparent medium:dark:bg-transparent medium:flex-row medium:whitespace-nowrap`}
-            ></motion.div>
+              className={`${styling.smalleOverView} medium:relative medium:top-0 medium:border-0 medium:bg-transparent medium:dark:bg-transparent medium:flex-row medium:whitespace-nowrap medium:hidden`}
+            >
+              <CreateBtn handleBackToEdit={handleBackToEdit} />
+            </motion.div>
           ) : null}
         </AnimatePresence>
+
+        <CreateBtn
+          handleBackToEdit={handleBackToEdit}
+          classname={`hidden ${styling.smalleOverView} medium:relative medium:top-0 medium:border-0 medium:bg-transparent medium:dark:bg-transparent medium:flex-row medium:whitespace-nowrap medium:inline-flex`}
+        />
       </div>
     </header>
   );
