5f8005416b61255792bd5a3a253c3c641915eb85
Walker-Gideon <gideonawortwe3@gmail.com>
2025-08-10 04:49:19 +0000

    Working on the note createHeader

--- src/features/note/noteRight/createNote/CreateNoteHeader.jsx
+++ src/features/note/noteRight/createNote/CreateNoteHeader.jsx
@@ -1,9 +1,38 @@
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "../../../../firebase";
+import { v4 as uuidv4 } from "uuid";
 import { useNote } from "../../../../context/NoteContext";
 import Button from "../../../../ui/Button";
+import { useAuth } from "../../../../context/AuthContext";
 
 export default function CreateNoteHeader() {
-  const { title, content, isSubmittingNote, setAddNoteTitle, noteName } =
-    useNote();
+  const {
+    title,
+    content,
+    isSubmittingNote,
+    setAddNoteTitle,
+    noteName,
+    currentNote,
+  } = useNote();
+  const { user } = useAuth();
+
+  async function saveNote() {
+    if (!currentNote.title || !currentNote.content) return;
+
+    try {
+      await setDoc(
+        doc(db, "users", user.uid, "notes", currentNote.id || uuidv4()),
+        {
+          title: currentNote.title,
+          content: currentNote.content,
+          updatedAt: new Date(),
+        },
+      );
+      console.log("Note saved successfully!");
+    } catch (error) {
+      console.error("Error saving note:", error);
+    }
+  }
 
   function handleSaveNote(e) {
     e.preventDefault();
