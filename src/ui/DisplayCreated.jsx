e4d443793e09ce50e733acea633a86d6ad1c0806
Walker-Gideon <gideonawortwe3@gmail.com>
2025-08-11 00:58:23 +0000

    Added the deleteNOte logic

--- src/ui/DisplayCreated.jsx
+++ src/ui/DisplayCreated.jsx
@@ -1,8 +1,35 @@
-import { LuEllipsisVertical } from "react-icons/lu";
+import { deleteDoc, doc } from "firebase/firestore";
+import { db, auth } from "../firebase";
 import { RiDeleteBin5Line } from "react-icons/ri";
 import Button from "./Button";
+import { useNote } from "../context/NoteContext";
 
 export default function DisplayCreated({ title, timing }) {
+  const { setNotes } = useNote();
+
+  const handleDeleteNote = async (noteId) => {
+    const confirmDelete = window.confirm(
+      "Are you sure you want to delete this note? This action cannot be undone.",
+    );
+
+    if (!confirmDelete) return;
+
+    const user = auth.currentUser;
+    if (!user) return alert("User not logged in");
+
+    try {
+      await deleteDoc(doc(db, "users", user.uid, "notes", noteId));
+
+      // Update UI without refetching
+      setNotes((prevNotes) => prevNotes.filter((note) => note.id !== noteId));
+
+      alert("Note deleted successfully!");
+    } catch (error) {
+      console.error("Error deleting note:", error);
+      alert("Failed to delete note");
+    }
+  };
+
   return (
     <div
       role="button"
@@ -20,7 +47,7 @@ export default function DisplayCreated({ title, timing }) {
 
       <Button
         variant="outline"
-        onClick={() => {}}
+        onClick={handleDeleteNote}
         classname="text-slate-700 dark:text-slate-200"
       >
         <RiDeleteBin5Line className="h-5 w-5" />
