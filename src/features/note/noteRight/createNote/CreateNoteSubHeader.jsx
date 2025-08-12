646cd200c3513db34f37c9d1914bf528e58de9fc
Walker-Gideon <gideonawortwe3@gmail.com>
2025-08-12 08:13:56 +0000

    Refactor my button code

--- src/features/note/noteRight/createNote/CreateNoteSubHeader.jsx
+++ src/features/note/noteRight/createNote/CreateNoteSubHeader.jsx
@@ -1,145 +1,99 @@
-import { useState } from "react";
 import Button from "../../../../ui/Button";
 import { LuAlignLeft } from "react-icons/lu";
 import { LuAlignJustify } from "react-icons/lu";
 import { LuAlignRight } from "react-icons/lu";
 import { LuAlignCenter } from "react-icons/lu";
 
+const editingTools = [
+  {
+    text: "H1",
+    style: "px-2.5",
+    activeCheck: (editor) => editor.isActive("heading", { level: 1 }),
+    command: (editor) =>
+      editor.chain().focus().toggleHeading({ level: 1 }).run(),
+  },
+  {
+    text: "H2",
+    activeCheck: (editor) => editor.isActive("heading", { level: 2 }),
+    command: (editor) =>
+      editor.chain().focus().toggleHeading({ level: 2 }).run(),
+  },
+  {
+    text: "B",
+    style: "font-bold px-[13px]",
+    activeCheck: (editor) => editor.isActive("bold"),
+    command: (editor) => editor.chain().focus().toggleBold().run(),
+  },
+  {
+    text: "I",
+    style: "italic px-[14px]",
+    activeCheck: (editor) => editor.isActive("italic"),
+    command: (editor) => editor.chain().focus().toggleItalic().run(),
+  },
+  {
+    text: "U",
+    style: "underline px-[11px]",
+    activeCheck: (editor) => editor.isActive("underline"),
+    command: (editor) => editor.chain().focus().toggleUnderline().run(),
+  },
+  {
+    text: "H",
+    style: "px-[11px]",
+    activeCheck: (editor) => editor.isActive("highlight"),
+    command: (editor) => editor.chain().focus().toggleHighlight().run(),
+  },
+];
+
+const textAlignButtons = [
+  { align: "left", icon: LuAlignLeft },
+  { align: "center", icon: LuAlignCenter },
+  { align: "right", icon: LuAlignRight },
+  { align: "justify", icon: LuAlignJustify },
+];
+
 export default function CreateNoteSubHeader({ editor }) {
-  const [select, setSelect] = useState("h1");
-
-  const base =
-    "font-medium text-sm border p-2 rounded-sm cursor-pointer border-stone-300 dark:border-slate-700 transition-all duration-200";
-  const primColors = "bg-slate-500 text-white hover:bg-slate-600";
-  const secColors =
-    "text-slate-900  hover:text-white dark:text-white hover:bg-slate-600";
-
   const styling = {
-    h1: base + ` px-2.5 ${select === "h1" ? `${primColors}` : `${secColors}`}`,
-    h2: base + ` ${select === "h2" ? `${primColors}` : `${secColors}`}`,
-    bold:
-      base +
-      ` font-bold px-3 ${select === "bold" ? `${primColors}` : `${secColors}`}`,
-    italic:
-      base +
-      ` italic px-3.5 ${select === "italic" ? `${primColors}` : `${secColors}`}`,
-    underline:
-      base +
-      ` underline px-3 ${select === "underline" ? `${primColors}` : `${secColors}`}`,
+    base: "font-medium text-sm border p-2 rounded-sm cursor-pointer border-stone-300 dark:border-slate-700 transition-all duration-200",
+    isActive: "bg-slate-500 text-white hover:bg-slate-600",
+    notActive:
+      "text-slate-900 hover:text-white dark:text-white hover:bg-slate-600",
   };
 
   return (
     <div
       className={`mx-4 flex h-16 items-center gap-2 border-b border-stone-300 dark:border-slate-700`}
     >
-      <Button
-        variant="outline"
-        classname={editor.isActive("heading", { level: 1 }) ? "is-active" : ""}
-        onClick={(e) => {
-          e.preventDefault();
-          editor.chain().focus().toggleHeading({ level: 1 }).run();
-        }}
-      >
-        H1
-      </Button>
+      {editingTools.map((data, index) => (
+        <Button
+          key={index}
+          variant="outline"
+          classname={`${data.style} ${styling.base} ${data.activeCheck(editor) ? `${styling.isActive}` : `${styling.notActive}`}`}
+          onClick={(e) => {
+            e.preventDefault();
+            data.command(editor);
+          }}
+        >
+          {data.text}
+        </Button>
+      ))}
 
-      <Button
-        variant="outline"
-        classname={editor.isActive("heading", { level: 2 }) ? "is-active" : ""}
-        onClick={(e) => {
-          e.preventDefault();
-          editor.chain().focus().toggleHeading({ level: 2 }).run();
-        }}
-      >
-        H2
-      </Button>
-
-      <Button
-        variant="outline"
-        classname={editor.isActive("bold") ? "is-active" : ""}
-        onClick={(e) => {
-          e.preventDefault();
-          editor.chain().focus().toggleBold().run();
-        }}
-      >
-        B
-      </Button>
-
-      <Button
-        variant="outline"
-        classname={editor.isActive("italic") ? "is-active" : ""}
-        onClick={(e) => {
-          e.preventDefault();
-          editor.chain().focus().toggleItalic().run();
-        }}
-      >
-        I
-      </Button>
-
-      <Button
-        variant="outline"
-        classname={editor.isActive("underline") ? "is-active" : ""}
-        onClick={(e) => {
-          e.preventDefault();
-          editor.chain().focus().toggleUnderline().run();
-        }}
-        title="Underline (Ctrl+U)"
-      >
-        U
-      </Button>
-
-      <Button
-        variant="outline"
-        onClick={(e) => {
-          e.preventDefault();
-          editor.chain().focus().toggleHighlight().run();
-        }}
-        classname={editor.isActive("highlight") ? "is-active" : ""}
-      >
-        H
-      </Button>
-
-      {/* Alignments */}
-      <Button
-        variant="outline"
-        onClick={(e) => {
-          e.preventDefault();
-          editor.chain().focus().setTextAlign("left").run();
-        }}
-        classname={editor.isActive({ textAlign: "left" }) ? "is-active" : ""}
-      >
-        <LuAlignLeft />
-      </Button>
-      <Button
-        variant="outline"
-        onClick={(e) => {
-          e.preventDefault();
-          editor.chain().focus().setTextAlign("center").run();
-        }}
-        classname={editor.isActive({ textAlign: "center" }) ? "is-active" : ""}
-      >
-        <LuAlignCenter />
-      </Button>
-      <Button
-        variant="outline"
-        onClick={(e) => {
-          e.preventDefault();
-          editor.chain().focus().setTextAlign("right").run();
-        }}
-        classname={editor.isActive({ textAlign: "right" }) ? "is-active" : ""}
-      >
-        <LuAlignRight />
-      </Button>
-      <Button
-        variant="outline"
-        onClick={(e) => {
-          e.preventDefault();
-          editor.chain().focus().setTextAlign("justify").run();
-        }}
-        classname={editor.isActive({ textAlign: "justify" }) ? "is-active" : ""}
-      >
-        <LuAlignJustify />
-      </Button>
+      {textAlignButtons.map((btn, index) => (
+        <Button
+          key={index}
+          variant="outline"
+          classname={`px-[9.5px] py-[11px] ${styling.base} ${
+            editor.isActive({ textAlign: btn.align })
+              ? `${styling.isActive}`
+              : `${styling.notActive}`
+          }`}
+          onClick={(e) => {
+            e.preventDefault();
+            editor.chain().focus().setTextAlign(btn.align).run();
+          }}
+        >
+          <btn.icon />
+        </Button>
+      ))}
     </div>
   );
 }
