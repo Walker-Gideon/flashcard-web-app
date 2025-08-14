43f664eb6cb9af8285e6f37cb7460b907c126ed3
Walker-Gideon <gideonawortwe3@gmail.com>
2025-08-14 10:24:24 +0000

    Added the logic to display between the has flashcard screen and the initial screen

--- src/features/flashcard/FlashcardInit.jsx
+++ src/features/flashcard/FlashcardInit.jsx
@@ -1,13 +1,31 @@
+import { useEffect, useState } from "react";
+import { collection, onSnapshot } from "firebase/firestore";
+import { db } from "../../firebase";
+import { useAuth } from "../../context/AuthContext";
+import { useFlash } from "../../context/FlashcardContext";
 import { LuNotebookText } from "react-icons/lu";
 import Button from "../../ui/Button";
 import PromptDisplay from "../../ui/PromptDisplay";
 import FlashcardHeader from "./FlashcardHeader";
-import { useFlash } from "../../context/FlashcardContext";
 import useLazyLoading from "../../ui/LazyLoading";
 
 export default function FlashcardInit() {
+  const { user } = useAuth();
   const { setShowCreateFlashcard } = useFlash();
+  const [hasFlashcard, setHasFlashcard] = useState(false);
   const lazyLoading = useLazyLoading(setShowCreateFlashcard, 1000);
+
+  useEffect(() => {
+    if (!user?.uid) return;
+
+    const flashcardRef = collection(db, "users", user.uid, "flashcards");
+
+    const unsubscribe = onSnapshot(flashcardRef, (snapshot) => {
+      setHasFlashcard(snapshot.size > 0);
+    });
+
+    return () => unsubscribe();
+  }, [user]);
 
   function handleCreateFlashcard() {
     lazyLoading(true);
@@ -15,33 +33,38 @@ export default function FlashcardInit() {
 
   return (
     <div>
-      <FlashcardHeader text="My Flashcards" />
-
-      <div className="h-full">
-        <PromptDisplay
-          hight={"h-[80dvh]"}
-          textPrim="You haven't created any flashcards yet."
-          textSec={
-            <span className="flex min-w-sm flex-col items-center">
-              'Get started by tapping "Create Flashcard".'
-            </span>
-          }
-          icon={
-            <LuNotebookText className="h-6 w-6 text-slate-600 dark:text-slate-300" />
-          }
-        >
-          <Button
-            variant="primary"
-            classname={"mt-2 py-2 border-0"}
-            color={
-              "bg-slate-500 text-white hover:bg-slate-600 focus:ring-slate-300"
-            }
-            onClick={handleCreateFlashcard}
-          >
-            Create Flashcard
-          </Button>
-        </PromptDisplay>
-      </div>
+      {hasFlashcard ? (
+        <div className=""> have flashcard</div>
+      ) : (
+        <>
+          <FlashcardHeader text="My Flashcards" />
+          <div className="h-full">
+            <PromptDisplay
+              hight={"h-[80dvh]"}
+              textPrim="You haven't created any flashcards yet."
+              textSec={
+                <span className="flex min-w-sm flex-col items-center">
+                  'Get started by tapping "Create Flashcard".'
+                </span>
+              }
+              icon={
+                <LuNotebookText className="h-6 w-6 text-slate-600 dark:text-slate-300" />
+              }
+            >
+              <Button
+                variant="primary"
+                classname={"mt-2 py-2 border-0"}
+                color={
+                  "bg-slate-500 text-white hover:bg-slate-600 focus:ring-slate-300"
+                }
+                onClick={handleCreateFlashcard}
+              >
+                Create Flashcard
+              </Button>
+            </PromptDisplay>
+          </div>{" "}
+        </>
+      )}
     </div>
   );
 }
