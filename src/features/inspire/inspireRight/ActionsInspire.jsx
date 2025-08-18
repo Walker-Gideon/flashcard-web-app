6b7f953154fc38311449ab91c015274802cabf0d
Walker-Gideon <gideonawortwe3@gmail.com>
2025-08-18 03:10:24 +0000

    Added the logic for the inspire action

--- src/features/inspire/inspireRight/ActionsInspire.jsx
+++ src/features/inspire/inspireRight/ActionsInspire.jsx
@@ -3,8 +3,29 @@ import { LuChartColumnIncreasing } from "react-icons/lu";
 import CardOverview from "../../../ui/CardOverview";
 import HeaderText from "../../../ui/HeaderText";
 import Button from "../../../ui/Button";
+import useLoaderAction from "../../../utils/LoaderAction";
+import { useNav } from "../../../context/NavigateContext";
+import UserWelcome from "../../user/UserWelcome";
+
+const actionsData = [
+  {
+    icon: LuBookOpen,
+    text: "Review Cards",
+    to: "/dashboard/flashcards",
+    title: "My Flashcards",
+  },
+  {
+    icon: LuChartColumnIncreasing,
+    text: "View Detailed Analytics",
+    to: "/dashboard",
+    title: <UserWelcome userDisplay={true} />,
+  },
+];
 
 export default function ActionsInspire() {
+  const { setNavigateTitle, setNavShowOverLay, setShowSidebar } = useNav();
+  const navigate = useLoaderAction(1000);
+
   const styling = {
     button: "primaryButton flex w-full medium:text-base items-center space-x-3",
     icon: "h-5 w-5",
@@ -13,16 +34,24 @@ export default function ActionsInspire() {
   return (
     <CardOverview classname="mb-19">
       <HeaderText classname="mb-4">Quick Actions</HeaderText>
-      <div className="space-y-3">
-        <Button variant="outline" classname={styling.button}>
-          <LuBookOpen className={styling.icon} />
-          <span>Review Cards</span>
-        </Button>
-        <Button variant="outline" classname={styling.button}>
-          <LuChartColumnIncreasing className={styling.icon} />
-          <span>View Detailed Analytics</span>
-        </Button>
-      </div>
+
+      {actionsData.map((data, index) => (
+        <div key={index} className={`space-y-3 ${index === 0 ? "mb-2" : ""}`}>
+          <Button
+            variant="outline"
+            classname={styling.button}
+            onClick={() => {
+              navigate(data.to);
+              setNavShowOverLay((show) => !show);
+              setShowSidebar((show) => !show);
+              setNavigateTitle(data.title);
+            }}
+          >
+            <data.icon className={styling.icon} />
+            <span>{data.text}</span>
+          </Button>
+        </div>
+      ))}
     </CardOverview>
   );
 }
--- src/features/user/UserWelcome.jsx
+++ src/features/user/UserWelcome.jsx
@@ -1,6 +1,25 @@
+import { useAuth } from "../../context/AuthContext";
 import HeaderText from "../../ui/HeaderText";
 
-export default function UserWelcome({ title, subText, show }) {
+export default function UserWelcome({ title, subText, show, userDisplay }) {
+  const { userData } = useAuth();
+
+  const displayName =
+    userData.username &&
+    userData.username.charAt(0).toUpperCase() + userData.username.slice(1);
+
+  if (userDisplay)
+    return (
+      <div>
+        <HeaderText>{`Welcome, ${displayName ? displayName : "Username"}!`}</HeaderText>
+        <p
+          className={`text-sm text-slate-500 dark:text-slate-400 ${show ? "medium:block hidden" : ""}`}
+        >
+          Glad to have you on board.
+        </p>
+      </div>
+    );
+
   return (
     <div>
       <HeaderText>{title}</HeaderText>
