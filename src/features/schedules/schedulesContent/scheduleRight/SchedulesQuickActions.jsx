8bc6fcbecc8094a10fa4b2a61c4e00cb05ac62f7
Walker-Gideon <gideonawortwe3@gmail.com>
2025-08-24 16:40:43 +0000

    Adjustment to the scheduleQuickAction component

--- src/features/schedules/schedulesContent/scheduleRight/SchedulesQuickActions.jsx
+++ src/features/schedules/schedulesContent/scheduleRight/SchedulesQuickActions.jsx
@@ -1,38 +1,51 @@
 import { LuPlay } from "react-icons/lu";
 import { LuPlus } from "react-icons/lu";
-import { LuBrain } from "react-icons/lu";
 import { LuZap } from "react-icons/lu";
 import CardOverview from "../../../../ui/CardOverview";
 import CardHeader from "../../../../ui/CardHeader";
 import Button from "../../../../ui/Button";
+import useLoaderAction from "../../../../utils/LoaderAction";
+
+const actionsData = [
+  {
+    icon: LuPlay,
+    text: "Start Next Session",
+    to: "",
+  },
+  {
+    icon: LuPlus,
+    text: "Schedule Session",
+    popAction: "",
+  },
+  {
+    icon: LuZap,
+    text: "Quick Review",
+    to: "/dashboard/flashcards",
+  },
+];
 
 export default function SchedulesQuickActions() {
-  const styling =
-    "w-full button flex items-center border-0 space-x-3 p-3 bg-slate-500 text-white hover:bg-slate-600 focus:ring-slate-300";
+  const navigate = useLoaderAction(1000);
 
   return (
     <CardOverview>
       <CardHeader title="Quick Actions"></CardHeader>
       <div className="space-y-3">
-        <Button variant="outline" classname={styling}>
-          <LuPlay className="h-5 w-5" />
-          <span>Start Next Session</span>
-        </Button>
-
-        <Button variant="outline" classname={styling}>
-          <LuPlus className="h-5 w-5" />
-          <span>Schedule Session</span>
-        </Button>
-
-        <Button variant="outline" classname={styling}>
-          <LuBrain className="h-5 w-5" />
-          <span>Smart Schedule</span>
-        </Button>
-
-        <Button variant="outline" classname={styling}>
-          <LuZap className="h-5 w-5" />
-          <span>Quick Review</span>
-        </Button>
+        {actionsData.map((data, index) => (
+          <Button
+            key={index}
+            onClick={() => {
+              navigate(data.to);
+            }}
+            variant="outline"
+            classname={
+              "w-full button flex items-center border-0 space-x-3 p-3 bg-slate-500 text-white hover:bg-slate-600 focus:ring-slate-300"
+            }
+          >
+            <data.icon className="h-5 w-5" />
+            <span>{data.text}</span>
+          </Button>
+        ))}
       </div>
     </CardOverview>
   );
