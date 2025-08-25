4d6234bd473cdc6277884bb3dc6c6dc71ffd05eb
Walker-Gideon <gideonawortwe3@gmail.com>
2025-08-25 04:42:44 +0000

    Added the session data to it component

--- src/features/schedules/session/SessionDate.jsx
+++ src/features/schedules/session/SessionDate.jsx
@@ -1,5 +1,52 @@
-import React from "react";
+import { LuCalendar } from "react-icons/lu";
+import { LuClock } from "react-icons/lu";
+import { useGen } from "../../../context/GeneralContext";
+import Input from "../../../ui/Input";
 
 export default function SessionDate() {
-  return <div></div>;
+  const { formData, setFormData, isSubmitting } = useGen();
+
+  const inputStyling = `w-full dark:text-white pl-10  disabled:cursor-not-allowed`;
+
+  return (
+    <div className="grid grid-cols-2 gap-3">
+      {/* Date */}
+      <div>
+        <label className="mb-2 block text-sm font-medium text-slate-700 dark:text-slate-300">
+          Date *
+        </label>
+        <div className="relative">
+          <LuCalendar className="absolute top-1/2 left-3 h-4 w-4 -translate-y-1/2 text-slate-400" />
+          <Input
+            type="date"
+            classname={inputStyling}
+            disabled={isSubmitting}
+            value={formData.date}
+            onChange={(e) =>
+              setFormData((prev) => ({ ...prev, date: e.target.value }))
+            }
+          />
+        </div>
+      </div>
+
+      {/* Time */}
+      <div>
+        <label className="mb-2 block text-sm font-medium text-slate-700 dark:text-slate-300">
+          Time *
+        </label>
+        <div className="relative">
+          <LuClock className="absolute top-1/2 left-3 h-4 w-4 -translate-y-1/2 text-slate-400" />
+          <Input
+            type="time"
+            classname={inputStyling}
+            disabled={isSubmitting}
+            value={formData.time}
+            onChange={(e) =>
+              setFormData((prev) => ({ ...prev, time: e.target.value }))
+            }
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/features/schedules/session/SessionForm.jsx
+++ src/features/schedules/session/SessionForm.jsx
@@ -1,15 +1,15 @@
 import { LuTarget } from "react-icons/lu";
-import { LuCalendar } from "react-icons/lu";
-import { LuClock } from "react-icons/lu";
+
 import { LuPlus } from "react-icons/lu";
 import Input from "../../../ui/Input";
 import Button from "../../../ui/Button";
 import { useGen } from "../../../context/GeneralContext";
 import SessionSelectTag from "./SessionSelectTag";
 import SessionOptional from "./SessionOptional";
+import SessionDate from "./SessionDate";
 
 export default function SessionForm() {
-  const { setSessionModel, formData, setFormData, isSubmitting } = useGen();
+  const { setSessionModel, formData, isSubmitting } = useGen();
 
   const inputStyling = `w-full dark:text-white pl-10  disabled:cursor-not-allowed`;
 
@@ -36,45 +36,7 @@ export default function SessionForm() {
       </div>
 
       {/* Date and Time Row */}
-      <div className="grid grid-cols-2 gap-3">
-        {/* Date */}
-        <div>
-          <label className="mb-2 block text-sm font-medium text-slate-700 dark:text-slate-300">
-            Date *
-          </label>
-          <div className="relative">
-            <LuCalendar className="absolute top-1/2 left-3 h-4 w-4 -translate-y-1/2 text-slate-400" />
-            <Input
-              type="date"
-              classname={inputStyling}
-              disabled={isSubmitting}
-              value={formData.date}
-              onChange={(e) =>
-                setFormData((prev) => ({ ...prev, date: e.target.value }))
-              }
-            />
-          </div>
-        </div>
-
-        {/* Time */}
-        <div>
-          <label className="mb-2 block text-sm font-medium text-slate-700 dark:text-slate-300">
-            Time *
-          </label>
-          <div className="relative">
-            <LuClock className="absolute top-1/2 left-3 h-4 w-4 -translate-y-1/2 text-slate-400" />
-            <Input
-              type="time"
-              classname={inputStyling}
-              disabled={isSubmitting}
-              value={formData.time}
-              onChange={(e) =>
-                setFormData((prev) => ({ ...prev, time: e.target.value }))
-              }
-            />
-          </div>
-        </div>
-      </div>
+      <SessionDate />
 
       {/* Estimated Time (Optional) */}
       <SessionOptional />
