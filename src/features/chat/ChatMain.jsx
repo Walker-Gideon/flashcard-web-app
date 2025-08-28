fe61722d83f66ebe08adb9b299ec0ed7f1a298e5
Walker-Gideon <gideonawortwe3@gmail.com>
2025-08-28 02:16:05 +0000

    Adjust the styling in the chat layout

--- src/features/chat/ChatMain.jsx
+++ src/features/chat/ChatMain.jsx
@@ -8,13 +8,9 @@ export default function ChatMain({ messagesEndRef, handleSubmit }) {
 
   return (
     <div className="relative h-screen flex-1 flex-col overflow-hidden p-1">
-      {/* chat-scrollbar overflow-y-auto */}
-      {/* flex-1 overflow-y-scroll h-[50] */}
-      <div className="scroll-container h-100 overflow-y-scroll bg-green-500 pr-2">
+      <div className="scroll-container medium:h-120 h-118 overflow-y-scroll pr-2">
         <RenderMessage messages={messages} />
         <TypingIndicator isTyping={isTyping} />
-
-        {/* Invisible div for auto-scroll */}
         <div ref={messagesEndRef} />
       </div>
 
--- src/features/chat/MessageInput.jsx
+++ src/features/chat/MessageInput.jsx
@@ -1,36 +1,37 @@
 import { LuSendHorizontal } from "react-icons/lu";
+import { useChat } from "../../context/ChatContext";
 
-export default function MessageInput({
-  handleSubmit,
-  inputMessage,
-  setInputMessage,
-  isTyping,
-}) {
+export default function MessageInput({ handleSubmit }) {
+  const { inputMessage, setInputMessage, isTyping } = useChat();
+
   return (
-    <div className="absolute w-full bottom-0 left-0 bg-yellow-500">
-    <form onSubmit={handleSubmit} className="mt-6 flex items-center space-x-2">
-      <textarea
-        value={inputMessage}
-        onChange={(e) => setInputMessage(e.target.value)}
-        placeholder="Type your question here..."
-        className="h-12 flex-1 resize-none overflow-hidden rounded-sm border border-stone-300 bg-slate-50 p-3 text-sm text-slate-900 focus:ring-2 focus:ring-slate-400 focus:outline-none dark:border-slate-700 dark:bg-slate-800 dark:text-white dark:focus:ring-slate-500"
-        rows={1}
-        onKeyDown={(e) => {
-          if (e.key === "Enter" && !e.shiftKey) {
-            e.preventDefault();
-            handleSubmit(e);
-          }
-        }}
-        disabled={isTyping}
-      />
-      <button
-        type="submit"
-        className="rounded-sm bg-slate-500 p-3 text-white transition-colors hover:bg-slate-600 disabled:cursor-not-allowed disabled:opacity-80"
-        disabled={inputMessage.trim() === "" || isTyping}
+    <div className="absolute bottom-1 left-0 w-full px-0.5">
+      <form
+        onSubmit={handleSubmit}
+        className="mt-6 flex items-center space-x-2"
       >
-        <LuSendHorizontal className="h-6 w-6" />
-      </button>
-    </form>
+        <textarea
+          value={inputMessage}
+          onChange={(e) => setInputMessage(e.target.value)}
+          placeholder="Type your question here..."
+          className="h-12 flex-1 resize-none overflow-hidden rounded-sm border border-stone-300 bg-slate-50 p-3 text-sm text-slate-900 focus:ring-2 focus:ring-slate-400 focus:outline-none dark:border-slate-700 dark:bg-slate-800 dark:text-white dark:focus:ring-slate-500"
+          rows={1}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" && !e.shiftKey) {
+              e.preventDefault();
+              handleSubmit(e);
+            }
+          }}
+          disabled={isTyping}
+        />
+        <button
+          type="submit"
+          className="rounded-sm bg-slate-500 p-3 text-white transition-colors hover:bg-slate-600 disabled:cursor-not-allowed disabled:opacity-80"
+          disabled={inputMessage.trim() === "" || isTyping}
+        >
+          <LuSendHorizontal className="h-6 w-6" />
+        </button>
+      </form>
     </div>
   );
 }
