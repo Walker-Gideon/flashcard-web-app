9588df161617380956bc409d780be0de404b1790
Walker-Gideon <gideonawortwe3@gmail.com>
2025-08-29 19:53:35 +0000

    Created the quction action reusable component

---
+++ src/ui/QuickAction.jsx
@@ -0,0 +1,14 @@
+import Button from "./Button";
+
+export default function QuickAction({ onClick, icon, text, styling }) {
+  return (
+    <Button
+      onClick={onClick}
+      variant="outline"
+      classname={`w-full button flex items-center border-0 space-x-3 p-3 bg-slate-500 text-white hover:bg-slate-600 focus:ring-slate-300 ${styling ? `${styling}` : ``}`}
+    >
+      {icon}
+      <span>{text}</span>
+    </Button>
+  );
+}
