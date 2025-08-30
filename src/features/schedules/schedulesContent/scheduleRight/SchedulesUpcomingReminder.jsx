8e80447cc7f8210ea840f83452dddedfb525d311
Walker-Gideon <gideonawortwe3@gmail.com>
2025-08-30 19:23:54 +0000

    Done with the schedule upcoming secton logic

--- src/features/schedules/schedulesContent/scheduleRight/SchedulesUpcomingReminder.jsx
+++ src/features/schedules/schedulesContent/scheduleRight/SchedulesUpcomingReminder.jsx
@@ -1,27 +1,10 @@
-import { doc, getDoc } from "firebase/firestore";
-import { db } from "../../../../firebase";
 import { LuClock } from "react-icons/lu";
 import CardOverview from "../../../../ui/CardOverview";
-import Button from "../../../../ui/Button";
 import { useGen } from "../../../../context/GeneralContext";
-import { useFlash } from "../../../../context/FlashcardContext";
-import { useAuth } from "../../../../context/AuthContext";
-import useLoaderAction from "../../../../utils/LoaderAction";
 
 export default function SchedulesUpcomingReminder() {
-  const { user } = useAuth();
-  const { todaySessions, sessions, flashcards } = useGen();
-  const {
-    displayCreatedFlashcard,
-    setQueryFlashcard,
-    setFilteredFlashcard,
-    setCurrentFlashcard,
-    setShowCreateFlashcard,
-    setShowPreview,
-    setReadAlredyFlashcard,
-    SetEditFlashcardData,
-  } = useFlash();
-  const navigate = useLoaderAction(1000);
+  const { todaySessions } = useGen();
+
   const now = new Date();
   let nearestSessions = [];
 
@@ -45,11 +28,9 @@ export default function SchedulesUpcomingReminder() {
     );
   }
 
-  console.log("Nearest session(s):", nearestSessions.at(0));
-
   function getTimeRemaining(scheduledAt) {
     const now = new Date();
-    const targetTime = scheduledAt.toDate(); // assuming Firebase Timestamp
+    const targetTime = scheduledAt.toDate();
     const diffMs = targetTime - now;
 
     if (diffMs <= 0) return "Starting now";
@@ -61,64 +42,8 @@ export default function SchedulesUpcomingReminder() {
     return `${hours > 0 ? hours + "h " : ""}${minutes}m remaining`;
   }
 
-  async function handleScheduleSession(sessionId) {
-    const fullSessionToday = sessions.find(
-      (session) => session.id === sessionId,
-    );
-
-    if (!fullSessionToday) {
-      return;
-    }
-
-    const sessionTodayId = fullSessionToday.id;
-
-    if (!sessionTodayId) {
-      return;
-    }
-
-    try {
-      const sessionRef = doc(
-        db,
-        "users",
-        user.uid,
-        "schedules",
-        sessionTodayId,
-      );
-      const sessionsSnap = await getDoc(sessionRef);
-
-      if (sessionsSnap.exists()) {
-        const sessionData = sessionsSnap.data();
-
-        const matchedFlashcard = flashcards.find(
-          (f) => f.id === sessionData.tagId,
-        );
-        if (matchedFlashcard) {
-          setCurrentFlashcard({
-            id: sessionData.tagId,
-            completed: true,
-            ...matchedFlashcard,
-          });
-
-          setShowPreview(true);
-          setShowCreateFlashcard(true);
-          setReadAlredyFlashcard(true);
-
-          SetEditFlashcardData({ id: sessionData.tagId, ...matchedFlashcard });
-          navigate("/dashboard/flashcards");
-        }
-      }
-
-      setQueryFlashcard("");
-      setFilteredFlashcard(displayCreatedFlashcard);
-    } catch (error) {
-      console.error("Error fetching flashcards:", error);
-    }
-  }
-
-  console.log(todaySessions);
-
   return (
-    <div>
+    <>
       {todaySessions.length !== 0 && (
         <CardOverview classname="medium:col-span-2 mb-18 rounded-2xl dark:text-white lg:col-span-1 bg-slate-500">
           <div className="mb-3 flex items-center space-x-2">
@@ -126,28 +51,18 @@ export default function SchedulesUpcomingReminder() {
             <h3 className="font-semibold">Next Session</h3>
           </div>
 
-          {/* This data will come for the user */}
           <p className="mb-4 font-medium">
             {nearestSessions?.at(0)?.tag} starts in{" "}
             {getTimeRemaining(nearestSessions?.at(0)?.scheduledAt)}
           </p>
 
-          <div className="flex items-center justify-between">
-            <div>
-              {/* This data will come for the user */}
-              <p className="text-sm">
-                {nearestSessions?.at(0)?.count}{" "}
-                {nearestSessions?.at(0)?.count === 1 ? "card" : "cards"} •{" "}
-                {nearestSessions?.at(0)?.estimatedTime} min
-              </p>
-            </div>
-
-            <Button variant="outline" classname="primaryButton">
-              Reschedule
-            </Button>
-          </div>
+          <p className="text-sm">
+            {nearestSessions?.at(0)?.count}{" "}
+            {nearestSessions?.at(0)?.count === 1 ? "card" : "cards"} •{" "}
+            {nearestSessions?.at(0)?.estimatedTime} min
+          </p>
         </CardOverview>
       )}
-    </div>
+    </>
   );
 }
