bfcb37007b65117d7d8d432ad79bc31cd6196bba
Walker-Gideon <gideonawortwe3@gmail.com>
2025-09-01 01:32:26 +0000

    Adjust the dashboard quick action and done with that component

--- src/features/dashboard/dashContent/QuickAction.jsx
+++ src/features/dashboard/dashContent/QuickAction.jsx
@@ -1,7 +1,7 @@
 import CardOverview from "../../../ui/CardOverview";
 import { LuPlus } from "react-icons/lu";
 import { LuPlay } from "react-icons/lu";
-import { LuZap } from "react-icons/lu";
+import { LuLightbulb } from "react-icons/lu";
 import { LuCalendar } from "react-icons/lu";
 import useLoaderAction from "../../../utils/LoaderAction";
 
@@ -17,14 +17,14 @@ const initialActionData = [
     to: "flashcards",
   },
   {
-    icon: LuZap,
-    text: "AI Help",
-    to: "",
-  },
-  {
     icon: LuCalendar,
     text: "Schedule",
     to: "schedules",
+  },
+  {
+    icon: LuLightbulb,
+    text: "Inspire",
+    to: "inspire",
   },
 ];
 
@@ -42,12 +42,7 @@ export default function QuickAction() {
           <button
             key={index}
             onClick={() => {
-              if (data.popAction) {
-                navigate("/flashcards");
-                data.popAction();
-              } else if (data.to) {
-                navigate(data.to);
-              }
+              navigate(data.to);
             }}
             className="group flex cursor-pointer flex-col items-center space-y-3 rounded-xl bg-slate-50 p-6 transition-all duration-300 hover:scale-105 hover:bg-slate-100 hover:shadow-md dark:bg-slate-700/50 dark:hover:bg-slate-700"
           >
