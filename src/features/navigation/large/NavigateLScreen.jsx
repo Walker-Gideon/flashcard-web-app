e47bc80a3e2fbf20c1cfc1d4d8e88c32ceed1aa3
Walker-Gideon <gideonawortwe3@gmail.com>
2025-09-05 03:51:59 +0000

    Refactoring of code in the Navagation Large screen component

--- src/features/navigation/large/NavigateLScreen.jsx
+++ src/features/navigation/large/NavigateLScreen.jsx
@@ -1,17 +1,9 @@
 import { useNav } from "../../../context/NavigateContext";
-import { LuLayoutDashboard } from "react-icons/lu";
-import { LuBookOpen } from "react-icons/lu";
-import { LuFile } from "react-icons/lu";
-import { LuCalendar } from "react-icons/lu";
-import { LuLightbulb } from "react-icons/lu";
-import { LuMessageCircle } from "react-icons/lu";
-import { LuSettings2 } from "react-icons/lu";
+import { LuLayoutDashboard, LuBookOpen, LuFile, LuCalendar, LuLightbulb, LuSettings2 } from "react-icons/lu";
 import NavButton from "../NavButton";
 import ButtonNav from "../../../ui/ButtonNav";
-import Button from "../../../ui/Button";
 import LargeHeader from "./LargeHeader";
 import UserProfile from "./UserProfile";
-import { useChat } from "../../../context/ChatContext";
 
 const buttonsData = [
   {
@@ -29,9 +21,6 @@ const buttonsData = [
     to: "flashcards",
     icon: <LuFile />,
   },
-];
-
-const nextButtonsData = [
   {
     text: "Schedules",
     to: "schedules",
@@ -57,7 +46,6 @@ export default function NavigateLScreen() {
     resize,
     setNavigateTitle,
   } = useNav();
-  const { setIsChatShow } = useChat();
 
   function handleClick(title) {
     setNavShowOverLay((show) => !show);
@@ -78,39 +66,6 @@ export default function NavigateLScreen() {
           <div className="w-full px-2">
             <div className={`flex flex-col`}>
               {buttonsData.map((data, index) => (
-                <NavButton
-                  key={index}
-                  text={data.text}
-                  to={data.to}
-                  icon={data.icon}
-                  onClick={() => handleClick(data.text)}
-                />
-              ))}
-            </div>
-
-            {/* Ai button here   */}
-            <div className="flex w-full px-0.5">
-              <ButtonNav navLarge={true} text="Chat" classname="w-full">
-                <Button
-                  variant="outline"
-                  onClick={() => {
-                    // handleClick("Chat")
-                    setIsChatShow((show) => !show);
-                  }}
-                  classname={`navButton hover:bg-slate-600 hover:text-white dark:text-white  ${resize ? `flex items-start justify-start` : ``}`}
-                >
-                  <LuMessageCircle />
-                  <span
-                    className={`block text-xs ${resize ? `block` : `medium:hidden`}`}
-                  >
-                    Chat
-                  </span>
-                </Button>
-              </ButtonNav>
-            </div>
-
-            <div className="flex flex-col">
-              {nextButtonsData.map((data, index) => (
                 <NavButton
                   key={index}
                   text={data.text}
