8d0ffc6e33146071a8c4be6cfc1e4d076dd4fad3
Walker-Gideon <gideonawortwe3@gmail.com>
2025-09-08 05:06:09 +0000

    Refactor code in the ThemeCom component

--- src/ui/ThemeCom.jsx
+++ src/ui/ThemeCom.jsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from "react";
-import { updateDoc, doc } from "firebase/firestore";
-import { db } from "../../firebase";
 import { useNav } from "../context/NavigateContext";
 import { useAuth } from "../context/AuthContext";
 import Button from "./Button";
@@ -12,8 +10,8 @@ export default function ThemeCom() {
   const { resize } = useNav();
 
   const [isDarkMode, setIsDarkMode] = useState(() => {
-  if (typeof window !== "undefined" && id) {
-    const saved = localStorage.getItem(`darkMode_${id}`);
+  if (typeof window !== "undefined") {
+    const saved = localStorage.getItem("darkMode");
     if (saved !== null) return JSON.parse(saved);
     return window.matchMedia("(prefers-color-scheme: dark)").matches;
     }
@@ -25,23 +23,17 @@ export default function ThemeCom() {
   }, [isDarkMode]);
 
   const toggleDarkMode = (id) => {
-    if(!id) return;
-    const newMode = !isDarkMode;
-    setIsDarkMode(newMode);
-    localStorage.setItem(`darkMode_${id}`, JSON.stringify(newMode));
-
-    try{
-      await updateDoc(doc(db, "users", id), {darkMode: newMode})
-    } catch(err) {
-      console.error("Faild to save theme", err)
+    if(id) {
+      const newMode = !isDarkMode;
+      setIsDarkMode(newMode);
+      localStorage.setItem("darkMode", JSON.stringify(newMode));
     }
-    
   };
 
   return (
     <ButtonNav navLarge={true} text={`${isDarkMode ? "Light" : "Dark"} Theme`}>
       <Button
-        onClick={() => toggleDarkMode(userData?.uid)}
+        onClick={toggleDarkMode}
         variant="outline"
         classname={`navButton bg-slate-500 text-white hover:bg-slate-600 medium:px-3.5`}
       >
